453cedf711dacbf5a8f8de958b3e3ecbdf5edb5d
Zakee Shaikh <78408518+zakeeshaikh@users.noreply.github.com>
2021-07-10 19:42:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -27,6 +27,8 @@ function setup() {
   box3 = new Box(700, 100, 70, 70);
   box4 = new Box(700, 100, 70, 70);
   box5 = new Box(700, 100, 70, 70);
+  box22 = new Box(700, 100, 70, 70);
+  box23 = new Box(700, 100, 70, 70);
   box6 = new Box(700, 100, 70, 70);
   box7 = new Box(900, 100, 70, 70);
   box8 = new Box(900, 100, 70, 70);
@@ -34,6 +36,19 @@ function setup() {
   box10 = new Box(900, 100, 70, 70);
   box11 = new Box(900, 100, 70, 70);
   box12 = new Box(900, 100, 70, 70);
+  box21 = new Box(900, 100, 70, 70);
+  box24 = new Box(900, 100, 70, 70);
+  box13 = new Box(800,100,70,70)
+  box13 = new Box(800,100,70,70)
+  
+  box14 = new Box(800,100,70,70)
+  box15 = new Box(800,100,70,70)
+  box16 = new Box(800,100,70,70)
+  box17 = new Box(800,100,70,70)
+  box18 = new Box(800,100,70,70)
+  box19 = new Box(800,100,70,70)
+  box20 = new Box(800,100,70,70)
+  
   
 }
 
@@ -53,6 +68,18 @@ function draw() {
   box10.display();
   box11.display()
   box12.display();
+  box13.display();
+  box14.display();
+  box15.display();
+  box16.display();
+  box17.display();
+  box18.display();
+  box19.display();
+  box20.display();
+  box21.display();
+  box22.display();
+  box23.display();
+  box24.display()
 
   hero.display();
   rope.display();
